375e534e0c84331c1bbc88fedd86fff3ac2da321
School_Bozhinov <marogo142005@gmail.com>
2023-02-01 23:21:03 +0200

    [ADD] logo teams

--- cmd/teams/main.go
+++ cmd/teams/main.go
@@ -43,6 +43,7 @@ func CreateTeam(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 	team = models.Team{
 		Name:        parseTeam.Name,
 		Description: parseTeam.Description,
+		Logo:        "https://api.hacktues.com/api/image/" + parseTeam.Name,
 	}
 
 	if result := db.Omit("ProjectID", "InvitesID").Create(&team); result.Error != nil {
