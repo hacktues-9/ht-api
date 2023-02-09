24682fc94dd7e744fa1dec12febba68eed7f71ca
Bozhinov <marogo142005@gmail.com>
2023-02-09 11:14:44 +0200

    [FIX]

--- cmd/teams/main.go
+++ cmd/teams/main.go
@@ -749,7 +749,7 @@ func GetTeam(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 	// get team from db
 	var team models.GetTeamView
 	db.Table("team").Select("team.name, team.description, team.logo").Where("team.id = ?\n", teamID).Scan(&team)
-	db.Table("users").Select("users.id, concat(users.first_name, ' ', users.last_name) AS name, socials.profile_picture AS avatar, role.name AS role").Joins("JOIN info ON users.info_id = info.id").Joins("JOIN socials ON info.socials_id = socials.id").Joins("JOIN role ON role.id = users.role_id").Where("users.team_id = ? AND users.deleted_at IS NULL", teamID).Scan(&team.Members)
+	db.Table("users").Select("users.id, concat(users.first_name, ' ', users.last_name) AS name, socials.profile_picture AS avatar, role.name AS role").Joins("JOIN info ON users.info_id = info.id").Joins("JOIN socials ON info.socials_id = socials.id").Joins("JOIN role ON role.id = users.role_id").Where("users.team_id = ? AND users.deleted_at IS NULL", teamID).Order("role_id desc").Scan(&team.Members)
 
 	// get team technologies from db
 	var teamTechnologies []string
