e0f5932cd8ef1c3cd41fc578cffacd225d7dc90d
Bozhinov <marogo142005@gmail.com>
2023-02-10 20:10:09 +0200

    [FIX]

--- cmd/admins/main.go
+++ cmd/admins/main.go
@@ -3,6 +3,7 @@ package admins
 import (
 	"encoding/json"
 	"fmt"
+	users2 "github.com/hacktues-9/API/cmd/users"
 	"github.com/hacktues-9/API/pkg/models"
 	"gorm.io/gorm"
 	"net/http"
@@ -18,9 +19,24 @@ func GetTeams(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 }
 
 func SearchWithFilters(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
+	sub, err := users2.ReturnAuthID(w, r, db)
+	if err != nil {
+		fmt.Printf("[ ERROR ] [ SearchWithFilters ] auth: %v\n", err)
+		models.RespHandler(w, r, models.DefaultNegResponse(http.StatusInternalServerError, "auth: "+err.Error(), 0), err, http.StatusInternalServerError, "AdminSearchWithFilters")
+		return
+	}
+
+	//get roleId
+	var roleId int
+	db.Table("users").Select("role_id").Where("id = ?", sub).Find(&roleId)
+	if roleId != 2 {
+		fmt.Printf("[ ERROR ] [ SearchWithFilters ] unauthorized: %v\n", err)
+		models.RespHandler(w, r, models.DefaultNegResponse(http.StatusUnauthorized, "unauthorized", 0), err, http.StatusUnauthorized, "AdminSearchWithFilters")
+		return
+	}
 	//parse request body
 	var filters models.ParseFilterUsers
-	err := json.NewDecoder(r.Body).Decode(&filters)
+	err = json.NewDecoder(r.Body).Decode(&filters)
 	if err != nil {
 		fmt.Printf("[ ERROR ] [ SearchWithFilters ] parse: %v\n", err)
 		models.RespHandler(w, r, models.DefaultNegResponse(http.StatusInternalServerError, "parse: "+err.Error(), 0), err, http.StatusInternalServerError, "AdminSearchWithFilters")
